469355c8b7f1f9afb59acd5f171a346ac39a2c54
Reinder Kamphorst <reinder@toubab.nl>
2021-11-14 11:19:37 +0100

    Add disclaimer to lambda-resolver index to indicate status of code

--- lambdacg-resolver/src/index.ts
+++ lambdacg-resolver/src/index.ts
@@ -8,6 +8,12 @@ import { AppSyncResolverEvent } from 'aws-lambda';
 const handlerFactories : HandlerFactory[] = require('./handlerFactories.json').map(require);
 const gateway = new Gateway(handlerFactories, executeAsync, compose);
 
+/**
+ * The following is a naïve implementation of a handler for appsync resolver events,
+ * only for illustrational purposes.
+ * Need som hands-on testing to find out if this will work. 
+ */
+
 function handleEventAsync (event : AppSyncResolverEvent<GatewayRequest,Record<string,any>>) : Promise<GatewayResponse>
 {
     return gateway.handleRequestAsync(event.arguments);
