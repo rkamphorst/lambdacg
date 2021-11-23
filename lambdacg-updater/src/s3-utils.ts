3decceefb28ba4680150ad50ee8fb5002ad52b2a
Reinder Kamphorst <reinder@toubab.nl>
2021-11-23 17:07:41 +0100

    Test S3-utils, run in actions.

--- lambdacg-updater/src/s3-utils.ts
+++ lambdacg-updater/src/s3-utils.ts
@@ -1,53 +1,41 @@
 
 import {S3} from "aws-sdk";
-import {URL} from "url";
 
-const s3 = new S3();
+const getS3TarballNamesAsync = async (s3Bucket:string, s3Prefix?:string) => {
+      const s3Client = new S3();
+      const result:string[] = [];
+      let marker:string = undefined;
 
-const getS3TarballUrlsAsync = (s3UrlString:string) => {
-    return new Promise((resolve, reject) => {
-      const s3 = new S3();
+      do {
+        const listObjectsResult = 
+          await s3Client.listObjects({
+            Bucket: s3Bucket,
+            Prefix: s3Prefix,
+            Delimiter: undefined,
+            EncodingType: 'url',
+            Marker: marker,
+            MaxKeys: 25
+          }).promise();
 
-      const s3Url = new URL(s3UrlString);
-      const { hostname: Bucket, pathname } = s3Url;
+        listObjectsResult.Contents
+          .map(({ Key }) => Key)
+          .filter(name => {
+            const filename = (s3Prefix ? name.substr(s3Prefix.length) : name);
 
-      const Prefix = pathname.substr(1);
+            return filename.indexOf('/') <= 0 &&
+              (name.endsWith('.tgz') || name.endsWith(".tar.gz") || name.endsWith(".tar"))
+          })
+          .forEach(name => result.push(name));
 
-      next(undefined,[]);
+        marker = 
+          listObjectsResult.IsTruncated ?
+            listObjectsResult.Contents[listObjectsResult.Contents.length - 1].Key :
+            undefined;
 
-      function next(Marker:string, contents:string[]) {
-        const params : S3.ListObjectsRequest = {
-          Bucket,
-          Prefix,
-          Delimiter: undefined,
-          EncodingType: 'url',
-          Marker,
-          MaxKeys: 1000,
-        };
-    
-        s3.listObjects(params, (err, data) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-    
-          const { Contents, IsTruncated } = data;
-    
-          Contents
-            .map(({ Key }) => `s3://${Bucket}/${Key}`)
-            .filter(url => url.endsWith('.tgz') || url.endsWith(".tar.gz") || url.endsWith(".tar"))
-            .forEach(url => contents.push(url));
-    
-          if (IsTruncated) {
-            const last = Contents[Contents.length - 1];
-            next(last.Key, contents);
-          } else {
-            resolve(contents);
-            return;
-          }
-        });
-      };
-    });
+      } while (!!marker);
+
+      return result;
+
   };
 
-  export {getS3TarballUrlsAsync};
+  export {getS3TarballNamesAsync};
---
+++ lambdacg-updater/test/s3-utils.test.ts
@@ -0,0 +1,83 @@
+
+import {S3} from "aws-sdk";
+import {v4 as uuid} from "uuid";
+import {getS3TarballNamesAsync} from "lambdacg-updater/s3-utils"
+import { expect } from "chai";
+
+describe("S3Utils", () => {
+
+    const s3Bucket:string = `lambdacgtest-${uuid()}`;
+    const s3Client:S3 = new S3();
+    const hasAwsCredentials = !!s3Client.config.credentials;
+    const packageFileNames = ["tgz.file.tgz", "tar.gz.file.tar.gz", "tar.file.tar"];
+    const packageFileNamesWithPrefix = ["prefix/tgz.file.tgz", "prefix/tar.gz.file.tar.gz", "prefix/tar.file.tar"];
+    const nonPackageFileNames = [...Array(73).keys()].map(() => uuid());
+    const nonPackageFileNamesWithPrefix = [...Array(79).keys()].map(() => `prefix/${uuid()}`);
+    const fileNames = [ 
+        ...packageFileNamesWithPrefix, 
+        ...nonPackageFileNames, 
+        ...packageFileNames,
+        ...nonPackageFileNamesWithPrefix
+     ];
+    
+    const if_awscreds = function (test: () => void|Promise<void>) {
+        return function() {
+            if (!hasAwsCredentials) {
+                this.skip();
+            } 
+            return test();
+        };
+    }
+
+    before(async () => {
+        if (hasAwsCredentials) {
+            await s3Client.createBucket({
+                Bucket: s3Bucket,
+                CreateBucketConfiguration: {
+                    LocationConstraint: "eu-west-1"
+                }
+            }).promise();
+            console.log(`Created bucket ${s3Bucket}`);
+
+            await Promise.all(
+                fileNames.map(fileName => 
+                    s3Client.putObject({ Bucket: s3Bucket, Key: fileName, Body: fileName}).promise()
+                    )
+                );
+            console.log(`Uploaded ${fileNames.length} files to bucket`)
+        } 
+    });
+
+    after(async () => {
+        if (hasAwsCredentials) {
+            const fileDeletePromises = [];
+            for (const fileName of fileNames) {
+                fileDeletePromises.push(s3Client.deleteObject({ Bucket: s3Bucket, Key: fileName }).promise());
+            }
+            await Promise.all(fileDeletePromises);
+            console.log(`Deleted ${fileNames.length} files from bucket`)
+
+            await s3Client.deleteBucket({ Bucket: s3Bucket}).promise();
+            console.log(`Deleted bucket ${s3Bucket}`);
+        }
+    });
+
+    describe("getS3TarballUrlsAsync", () => {
+
+        it("Should list package tarballs in the bucket with prefix 'prefix/'", if_awscreds(async () => {
+            const result = await getS3TarballNamesAsync(s3Bucket, "prefix/");
+            expect(result).to.have.deep.members(packageFileNamesWithPrefix);
+        }));
+
+        it("Should list package tarballs in the bucket with prefix 'prefix'", if_awscreds(async () => {
+            const result = await getS3TarballNamesAsync(s3Bucket, "prefix");
+            expect(result).to.have.deep.members(packageFileNamesWithPrefix);
+        }));
+
+        it("Should list package tarballs in the bucket without prefix", if_awscreds(async () => {
+            const result = await getS3TarballNamesAsync(s3Bucket);
+            expect(result).to.have.deep.members(packageFileNames);
+        }));
+    })
+
+});
