3b199aa3942fc463d5dd37fde4d794fd8f924272
Reinder Kamphorst <reinder@toubab.nl>
2021-12-14 22:33:28 +0100

    Complete coverage on lambdacg-updater/s3-utils

--- lambdacg-updater/test/s3-utils.test.ts
+++ lambdacg-updater/test/s3-utils.test.ts
@@ -12,6 +12,23 @@ const debugTest: (message: string) => void = (message) => {
     }
 };
 
+const packageFileNames = ["tgz.file.tgz", "tar.gz.file.tar.gz", "tar.file.tar"];
+const packageFileNamesWithPrefix = [
+    "prefix/tgz.file.tgz",
+    "prefix/tar.gz.file.tar.gz",
+    "prefix/tar.file.tar",
+];
+const nonPackageFileNames = [...Array(73).keys()].map(() => uuid());
+const nonPackageFileNamesWithPrefix = [...Array(79).keys()].map(
+    () => `prefix/${uuid()}`
+);
+const fileNames = [
+    ...packageFileNamesWithPrefix,
+    ...nonPackageFileNames,
+    ...packageFileNames,
+    ...nonPackageFileNamesWithPrefix,
+];
+
 describe("S3Utils", async function () {
     debugTestCallback = undefined;
     const awsTestSession = new AwsTestSession(
@@ -25,51 +42,29 @@ describe("S3Utils", async function () {
     this.timeout("15s");
     this.slow("10s");
 
-    before(async () => {
+    // S3 bucket to be used throughout the test
+    let s3Bucket: string | undefined = undefined;
+
+    before(async function () {
         await awsTestSession.initializeAsync();
+
+        if (awsTestSession.hasAwsCredentials()) {
+            const s3Contents: { [key: string]: string } = Object.assign(
+                {},
+                ...fileNames.map((fn) => ({ [fn]: fn }))
+            );
+            s3Bucket = await awsTestSession.createS3BucketWithContentsAsync(
+                s3Contents
+            );
+        }
     });
 
     after(async () => await awsTestSession.cleanupAsync());
 
     describe("S3Folder", function () {
-        const packageFileNames = [
-            "tgz.file.tgz",
-            "tar.gz.file.tar.gz",
-            "tar.file.tar",
-        ];
-        const packageFileNamesWithPrefix = [
-            "prefix/tgz.file.tgz",
-            "prefix/tar.gz.file.tar.gz",
-            "prefix/tar.file.tar",
-        ];
-        const nonPackageFileNames = [...Array(73).keys()].map(() => uuid());
-        const nonPackageFileNamesWithPrefix = [...Array(79).keys()].map(
-            () => `prefix/${uuid()}`
-        );
-        const fileNames = [
-            ...packageFileNamesWithPrefix,
-            ...nonPackageFileNames,
-            ...packageFileNames,
-            ...nonPackageFileNamesWithPrefix,
-        ];
-
-        let s3Bucket: string | undefined = undefined;
-
-        before(async function () {
-            if (awsTestSession.hasAwsCredentials()) {
-                const s3Contents: { [key: string]: string } = Object.assign(
-                    {},
-                    ...fileNames.map((fn) => ({ [fn]: fn }))
-                );
-                s3Bucket = await awsTestSession.createS3BucketWithContentsAsync(
-                    s3Contents
-                );
-            }
-        });
-
-        describe("constructor", function () {
+        describe("fromUrl", function () {
             it(
-                "Should throw when constructed for URL s3://[s3Bucket]/prefix",
+                "Should throw for URL s3://[s3Bucket]/prefix",
                 awsTestSession.withAws(async function () {
                     await expectToThrowAsync(() =>
                         S3Folder.fromUrl(
@@ -78,6 +73,18 @@ describe("S3Utils", async function () {
                             10
                         )
                     );
+                })
+            );
+            it(
+                "Should be successful for URL s3://[s3Bucket]/prefix/",
+                awsTestSession.withAws(async function () {
+                    const s3Folder = S3Folder.fromUrl(
+                        `s3://${s3Bucket}/prefix/`,
+                        awsTestSession.s3Client,
+                        10
+                    );
+                    expect(s3Folder).to.not.be.null;
+                    expect(s3Folder).to.be.instanceOf(S3Folder);
                 })
             );
         });
@@ -158,6 +165,135 @@ describe("S3Utils", async function () {
                     expect(result.map((o) => o.key)).to.have.deep.members(
                         packageFileNames
                     );
+                })
+            );
+        });
+    });
+
+    describe("S3Object", function () {
+        describe("fromUrlAndVersion", function () {
+            it(
+                "Should throw for URL s3://[s3Bucket]/object/key/",
+                awsTestSession.withAws(async function () {
+                    await expectToThrowAsync(() =>
+                        S3Object.fromUrlAndVersion(
+                            `s3://${s3Bucket}/object/key/`,
+                            "null",
+                            awsTestSession.s3Client
+                        )
+                    );
+                })
+            );
+            it(
+                "Should be successful for URL s3://[s3Bucket]/object/key",
+                awsTestSession.withAws(async function () {
+                    const s3Object = S3Object.fromUrlAndVersion(
+                        `s3://${s3Bucket}/object/key`,
+                        "null",
+                        awsTestSession.s3Client
+                    );
+                    expect(s3Object).to.not.be.null;
+                    expect(s3Object).to.be.instanceOf(S3Object);
+                })
+            );
+        });
+
+        describe("(set|get)TagsAsync", function () {
+            it(
+                "Should get the same tags as the ones set",
+                awsTestSession.withAws(async function () {
+                    const s3Folder = S3Folder.fromUrl(
+                        `s3://${s3Bucket}`,
+                        awsTestSession.s3Client,
+                        1000
+                    );
+                    const s3Object = (
+                        await s3Folder.listLatestObjectVersionsAsync(/.*/)
+                    )[0];
+
+                    await s3Object.setTagsAsync([
+                        { key: "a", value: "a-value" },
+                        { key: "b", value: "b-value" },
+                    ]);
+
+                    const gotTags = await s3Object.getTagsAsync();
+
+                    expect(gotTags).to.have.deep.members([
+                        { key: "a", value: "a-value" },
+                        { key: "b", value: "b-value" },
+                    ]);
+                })
+            );
+
+            it(
+                "Should fail if more than 10 tags set",
+                awsTestSession.withAws(async function () {
+                    const s3Folder = S3Folder.fromUrl(
+                        `s3://${s3Bucket}`,
+                        awsTestSession.s3Client,
+                        1000
+                    );
+                    const s3Object = (
+                        await s3Folder.listLatestObjectVersionsAsync(/.*/)
+                    )[0];
+
+                    expectToThrowAsync(
+                        async () =>
+                            await s3Object.setTagsAsync([
+                                { key: "0", value: "a-value" },
+                                { key: "1", value: "b-value" },
+                                { key: "2", value: "c-value" },
+                                { key: "3", value: "d-value" },
+                                { key: "4", value: "e-value" },
+                                { key: "5", value: "f-value" },
+                                { key: "6", value: "g-value" },
+                                { key: "7", value: "h-value" },
+                                { key: "8", value: "i-value" },
+                                { key: "9", value: "j-value" },
+                                { key: "10", value: "k-value" },
+                            ])
+                    );
+                })
+            );
+        });
+
+        describe("getDownloadStream", function () {
+            it(
+                "Should correctly download the contents of an object",
+                awsTestSession.withAws(async function () {
+                    const s3Folder = S3Folder.fromUrl(
+                        `s3://${s3Bucket}/prefix/`,
+                        awsTestSession.s3Client,
+                        1000
+                    );
+                    const s3Object = (
+                        await s3Folder.listLatestObjectVersionsAsync(/.*/)
+                    ).filter((x) => x.key == "prefix/tar.gz.file.tar.gz")[0];
+
+                    const stream = s3Object.getDownloadStream();
+
+                    const buffers: Buffer[] = [];
+
+                    const promise = new Promise((resolve, reject) => {
+                        stream.on("data", (data) => {
+                            buffers.push(data);
+                        });
+
+                        stream.on("end", (err: unknown) => {
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(
+                                    Buffer.concat(buffers).toString("utf-8")
+                                );
+                            }
+                        });
+                    });
+
+                    stream.resume();
+                    const result = await promise;
+
+                    expect(result).to.be.equal("prefix/tar.gz.file.tar.gz");
                 })
             );
         });
