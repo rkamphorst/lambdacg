2908a7280b23eebd2e2dd665f0f8ebff980d09cc
Reinder Kamphorst <reinder@toubab.nl>
2022-02-08 11:51:01 +0100

    Fix unit tests

--- lambdacg-updater/test/stream-utils.test.ts
+++ lambdacg-updater/test/stream-utils.test.ts
@@ -78,32 +78,74 @@ describe("Stream Utils", function () {
             // when read stream already finished
         });
 
-        it("Should throw when a read receives an error", async function () {
+        it("Should throw second time awaited when received an error", async function () {
+            const stream = new Readable({
+                read: () => {
+                    /*skip */
+                },
+            });
+            stream.destroy(new Error());
+
+            await expectToThrowAsync(
+                () => streamFinishedAsync(stream)
+            );
+
+            await expectToThrowAsync(
+                () => streamFinishedAsync(stream)
+            );
+        });
+
+        it("Should throw when a write receives an error", async function () {
+            const stream = new Writable({
+                write: () => {
+                    /*skip */
+                },
+            });
+            stream.destroy(new Error());
+
+            await expectToThrowAsync(
+                () => streamFinishedAsync(stream)
+            );
+
+            await expectToThrowAsync(
+                () => streamFinishedAsync(stream)
+            );
+        });
+    });
+
+    describeObject({ streamToStringAsync }, function () {
+        it("Should read the entire stream to a string", async function () {
+            const stream = new Readable({
+                read: () => {
+                    setTimeout(() => {
+                        stream.push("a");
+                        stream.push("b");
+                        stream.push("c");
+                        stream.push(null);
+                    }, 0);
+                },
+            });
+
+            const str = await streamToStringAsync(stream);
+
+            expect(str).to.be.equal("abc");
+        });
+
+        it("Should throw when the stream receives an error", async function () {
             const expectedError = new Error();
             const stream = new Readable({
                 read: () => {
                     throw expectedError;
                 },
             });
-            const promise = streamFinishedAsync(stream);
-            let emittedError: undefined | Error = undefined;
-
-            stream.on("data", () => {
-                /* skip */
-            });
-            stream.on("error", (e) => (emittedError = e));
-            setTimeout(() => {
-                stream.push(null);
-            }, 0);
 
             await expectToThrowAsync(
-                () => promise,
+                () => streamToStringAsync(stream),
                 (e) => e === expectedError
             );
-            expect(emittedError).to.be.equal(expectedError);
         });
 
-        it("Should not throw second time awaited when received an error", async function () {
+        it("Should throw when the stream has received an error", async function () {
             const expectedError = new Error();
             const stream = new Readable({
                 read: () => {
@@ -113,32 +155,9 @@ describe("Stream Utils", function () {
             stream.destroy(expectedError);
 
             await expectToThrowAsync(
-                () => streamFinishedAsync(stream),
+                () => streamToStringAsync(stream),
                 (e) => e === expectedError
             );
-
-            await expectToThrowAsync(
-                () => streamFinishedAsync(stream),
-                (e) => e === expectedError
-            );
-        });
-
-        it("Should throw when a write receives an error", async function () {
-            this.skip();
-        });
-    });
-
-    describeObject({ streamToStringAsync }, function () {
-        it("Should read the entire stream to a string", async function () {
-            this.skip();
-        });
-
-        it("Should throw when the stream receives an error", async function () {
-            this.skip();
-        });
-
-        it("Should throw when the stream has received an error", async function () {
-            this.skip();
         });
     });
 });
